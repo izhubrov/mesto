dc846906e05c8f1ec5a77f30d4a437b710cce6f4
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-01-15 22:21:19 +0300

    edit profile structure grid-->flex

--- script.js
+++ script.js
@@ -8,40 +8,33 @@ let btnSubmit = page.querySelector('.button_type_submit');
 //выше переменные для кнопок
 
 let profile = page.querySelector('.profile');
-let profileName = profile.querySelector('.profile__name');
-let profileAbout = profile.querySelector('.profile__about');
-//выше переменные для секции Profile
 
 //---Форма редактирования профиля---//
 let editForm = page.querySelector('.form_type_edit');
 let input = editForm.querySelector('.input');
-let editFormName = input.querySelector('.input__text_type_name');
-let editFormAbout = input.querySelector('.input__text_type_about');
+let profileName = profile.querySelector('.profile__name');
+let profileAbout = profile.querySelector('.profile__about');
+
 
 //выше переменные для Формы редактирования Profile
 
 function showEditForm() {
-  editFormName.setAttribute('value', profileName.textContent);
-  editFormAbout.setAttribute('value', profileAbout.textContent);
-  overlay.setAttribute('style','display:block');
-  editForm.setAttribute('style', 'display:block');
+  let editFormName = input.querySelector('.input__text_type_name');
+  let editFormAbout = input.querySelector('.input__text_type_about');
+  editFormName.value = profileName.textContent;
+  editFormAbout.value= profileAbout.textContent;
+  overlay.style.display = 'block';
+  editForm.style.display = 'block';
 }
 btnEdit.addEventListener('click', showEditForm);
 //Открыть Форму редактирования Profile
 
 function SubmitEditForm(evt) {
   evt.preventDefault();
-  /*let fn = input.querySelector('.input__text_type_name');
-  let fa = input.querySelector('.input__text_type_about');
-  let pn = profile.querySelector('.profile__name');
-  let pa = profile.querySelector('.profile__about');
-  let pnv = fn.getAttribute('value');
-  let pav = fa.value;*/
-  profileName = editFormName.nodeValue;
-  pn.textContent =  `${pnv}`;
-  pa.textContent =  `${pav}`;
-  console.log(`${pnv}`);
-  console.log(`${pav}`);
+  let editFormName = input.querySelector('.input__text_type_name');
+  let editFormAbout = input.querySelector('.input__text_type_about');
+  profileName.textContent =  `${editFormName.value}`;
+  profileAbout.textContent =  `${editFormAbout.value}`;
   closeEditForm(evt);
 }
 
@@ -54,8 +47,8 @@ editForm.addEventListener('submit',SubmitEditForm);
 
 function closeEditForm(evt) {
   evt.preventDefault();
-  overlay.setAttribute('style','display:none');
-  editForm.setAttribute('style', 'display:none');
+  overlay.style.display = 'none';
+  editForm.style.display = 'none';
 }
 btnClose.addEventListener('click', closeEditForm);
 //Закрыть Форму редактирования Profile*/
