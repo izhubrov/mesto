413642320f11f261544d3683453c046fc960b15c
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-03-14 17:34:15 +0300

    refactor: FormValidator.js

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -3,61 +3,56 @@ export default class FormValidator {
   constructor(validationSettings,formElement) {
     this._validationSettings = validationSettings;
     this._formElement = formElement;
+    this._buttonElement = this._formElement.querySelector(this._validationSettings.submitButtonSelector);
+    this._inputList = Array.from(this._formElement.querySelectorAll(this._validationSettings.inputSelector));
   }
 
-  _showInputError (fieldSet, inputElement, errorMessage) {
-    const errorElement = fieldSet.querySelector(`${this._validationSettings.errorSelector}_type_${inputElement.name}`);
+  _showInputError (inputElement, errorMessage) {
+    const errorElement = this._formElement.querySelector(`${this._validationSettings.errorSelector}_type_${inputElement.name}`);
     inputElement.classList.add(this._validationSettings.inputErrorClass);
     errorElement.textContent = errorMessage;
     errorElement.classList.add(this._validationSettings.activeErrorClass);
   };
 
-  _hideInputError (fieldSet, inputElement) {
-    const errorElement = fieldSet.querySelector(`${this._validationSettings.errorSelector}_type_${inputElement.name}`);
+  _hideInputError (inputElement) {
+    const errorElement = this._formElement.querySelector(`${this._validationSettings.errorSelector}_type_${inputElement.name}`);
     inputElement.classList.remove(this._validationSettings.inputErrorClass);
     errorElement.textContent = '';
     errorElement.classList.remove(this._validationSettings.activeErrorClass);
   }
 
-  _checkInputValidity (fieldSet, inputElement) {
+  _checkInputValidity (inputElement) {
     if (!inputElement.validity.valid) {
-      this._showInputError(fieldSet, inputElement, inputElement.validationMessage)
+      this._showInputError(inputElement, inputElement.validationMessage)
     } else {
-      this._hideInputError(fieldSet, inputElement)
+      this._hideInputError (inputElement)
     }
   };
 
-  _hasInvalidInput (inputList) {
-    return inputList.some((inputElement) => {
+  _hasInvalidInput () {
+    return this._inputList.some((inputElement) => {
       return !(inputElement.validity.valid && inputElement.value.trim());
     });
   }
 
-  _toggleButtonState (inputList, buttonElement) {
-    if (this._hasInvalidInput(inputList)) {
-      buttonElement.classList.add(this._validationSettings.inactiveButtonClass);
-      buttonElement.setAttribute('disabled',true);
+  _toggleButtonState () {
+    if (this._hasInvalidInput(this._inputList)) {
+      this._buttonElement.classList.add(this._validationSettings.inactiveButtonClass);
+      this._buttonElement.setAttribute('disabled',true);
     } else {
-      buttonElement.classList.remove(this._validationSettings.inactiveButtonClass);
-      buttonElement.removeAttribute('disabled',true);
+      this._buttonElement.classList.remove(this._validationSettings.inactiveButtonClass);
+      this._buttonElement.removeAttribute('disabled',true);
     }
   }
 
-  _setEventListeners (fieldSet) {
-    //Насколько я понимаю, inputList и buttonElement нельзя вынести в поле класса, потому что это fieldset, и по идее, таких
-    //наборов полей может быть несколько...И если привязывать здесь поиск к this._formElement, то
-    //тогда эти два набора полей станут зависимыми...А в форме может быть не один inputList и не одна кнопка...
-    //Если их вынести в свойства в конструктор, тогда если добавить новый fieldset в разметку, в этот this._formElement,
-    //тогда вторая кнопка не будет реагировать на валидацию ее inputList...
-    const inputList = Array.from(fieldSet.querySelectorAll(this._validationSettings.inputSelector));
-    const buttonElement = fieldSet.querySelector(this._validationSettings.submitButtonSelector);
-    this._toggleButtonState(inputList, buttonElement)
+  _setEventListeners () {
+    this._toggleButtonState()
 
-    inputList.forEach((inputElement) => {
+    this._inputList.forEach((inputElement) => {
       inputElement.classList.remove(this._validationSettings.inputErrorClass)
       inputElement.addEventListener('input', () => {
-        this._checkInputValidity(fieldSet, inputElement)
-        this._toggleButtonState(inputList, buttonElement)
+        this._checkInputValidity(inputElement)
+        this._toggleButtonState()
       });
     });
 
@@ -91,6 +86,6 @@ export default class FormValidator {
 
     const fieldSetList = Array.from(this._formElement.querySelectorAll(this._validationSettings.fieldSetSelector));
     
-    fieldSetList.forEach((fieldSet) => this._setEventListeners(fieldSet));
+    fieldSetList.forEach(() => this._setEventListeners());
   }
 }
