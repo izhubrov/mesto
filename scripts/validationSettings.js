e2ea37555f2dfdbfc69a6ff723a5c1d1fd1a8e21
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-03-24 18:33:03 +0300

    nothing

--- scripts/validationSettings.js
+++
@@ -1,9 +0,0 @@
-export const validationSettings = {
-  inputSelector: '.popup__input',
-  fieldSetSelector: '.popup__set',
-  errorSelector: '.popup__input-error',
-  submitButtonSelector: '.popup__btn-submit',
-  inactiveButtonClass: 'popup__btn-submit_inactive',
-  inputErrorClass: 'popup__input_type_error',
-  activeErrorClass: 'popup__input-error_active'
-}
